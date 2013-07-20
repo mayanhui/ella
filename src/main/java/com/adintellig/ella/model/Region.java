4f5e0b60b19c05adda397c7b9c39f6e2cc1f55a5
mayanhui <binma85@gmail.com>
2013-07-20 15:02:40 +0800

    add model region add table regions

---
+++ src/main/java/com/adintellig/ella/model/Region.java
@@ -0,0 +1,51 @@
+package com.adintellig.ella.model;
+
+import java.sql.Timestamp;
+
+public class Region {
+	private String regionName;
+	private Timestamp updateTime = null;
+
+	public String getRegionName() {
+		return regionName;
+	}
+
+	public void setRegionName(String regionName) {
+		this.regionName = regionName;
+	}
+
+	public Timestamp getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Timestamp updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((regionName == null) ? 0 : regionName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Region other = (Region) obj;
+		if (regionName == null) {
+			if (other.regionName != null)
+				return false;
+		} else if (!regionName.equals(other.regionName))
+			return false;
+		return true;
+	}
+
+}
