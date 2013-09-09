7b8cadb29431439b3c5f0834abdda0887efb7652
mayanhui <binma85@gmail.com>
2013-09-09 15:48:15 +0800

    change JAVA_OPTIONS for jetty.sh

--- jetty-server/bin/jetty.sh
+++ jetty-server/bin/jetty.sh
@@ -347,7 +347,7 @@ if [ -z "$JETTY_STATE" ]
 then
   JETTY_STATE=$JETTY_HOME/jetty.state
 fi
-JAVA_OPTIONS+=("-Xms80m -Xmx512m -XX:MaxPermSize=256m")
+JAVA_OPTIONS+=("-Xms128m -Xmx1024m -XX:MaxPermSize=512m")
 JAVA_OPTIONS+=("-Djetty.state=$JETTY_STATE")
 rm -f $JETTY_STATE
 
